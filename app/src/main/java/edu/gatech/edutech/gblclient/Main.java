3672bac28daed9e889b461598cd0f99f253ea132
Sumanto Biswas <sumanto@gatech.edu>
2018-04-01 22:02:03 -0700

    Get framework for travel to work

--- app/src/main/java/edu/gatech/edutech/gblclient/Main.java
+++ app/src/main/java/edu/gatech/edutech/gblclient/Main.java
@@ -1,5 +1,6 @@
 package edu.gatech.edutech.gblclient;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
@@ -11,7 +12,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONException;
+
 import edu.gatech.edutech.gblclient.utils.Service;
+import edu.gatech.edutech.gblclient.utils.Utility;
 
 public class Main extends AppCompatActivity {
 
@@ -83,6 +87,45 @@ public class Main extends AppCompatActivity {
                 // create and show the alert dialog
                 AlertDialog dialog = builder.create();
                 dialog.show();
+            }
+        });
+
+
+        textTravelAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence colors[] = new CharSequence[] {"red", "green", "blue", "black"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(thisObject);
+                builder.setTitle("Where do you want to travel to?");
+                builder.setItems(colors, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String flightNumber = "";
+                        String cityChoice = "";
+
+                        // setup the alert builder
+                        AlertDialog.Builder builder = new AlertDialog.Builder(thisObject);
+                        builder.setTitle("Travelling...");
+                        String flightText = "Your reservation has been confirmed for flight number " + flightNumber + " to " + cityChoice + ".";
+                        try {
+                            flightText += " " + Utility.getRandomData(Utility.convertJSONArrayToList(service.getGameMetadata().getJSONObject("flight").getJSONArray("line2")));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        builder.setMessage(flightText);
+
+                        // add a button
+                        builder.setPositiveButton("OK", null);
+
+                        // create and show the alert dialog
+                        AlertDialog newDialog = builder.create();
+                        newDialog.show();
+                    }
+                });
+
+                builder.show();
             }
         });
 
